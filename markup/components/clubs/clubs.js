cb8a9cbf24e07bc69bfc4eeb69f8bfdc9f747902
Olga Odnokolova <inc.belko@gmail.com>
2017-06-13 02:03:32 +0300

    only build folder

--- markup/components/clubs/clubs.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('.clubs__more').click(function() {
-        event.preventDefault();
-        $('.clubs__wrapper .one-club:nth-child(4), .clubs__wrapper .one-club:nth-child(5)').addClass('one-club--show');
-        $(this).hide();
-    });
-});
--- markup/components/text-block/text-block.js
+++
@@ -1,7 +0,0 @@
-var btnTextOpen  = document.querySelector(".js-btn-open");
-var textContainer = document.querySelector(".js-text-wrap");
-
-btnTextOpen.addEventListener("click", function() {
-    textContainer.classList.add("js-text-wrap--high");
-    btnTextOpen.classList.add("js-btn-open--closed");
-});
